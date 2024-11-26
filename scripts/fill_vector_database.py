9ad6100a02750d4e14db1e1f5cee9b477ee77b88
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2024-11-26 21:11:45 +0100

    Add images also to firebase when doing the fill vector db

--- scripts/fill_vector_database.py
+++ scripts/fill_vector_database.py
@@ -1,32 +1,44 @@
+import asyncio
 import os
 from pathlib import Path
 
+import pytest
 from dotenv import load_dotenv
+from fastapi import UploadFile
+from loguru import logger
 
 load_dotenv()
 
 from app.services.identify.image_vectorizer import ImageVectorizer
 from app.services.pinecone_container import PineconeContainer
-from app.shared.utils import read_img_from_path_with_mask
+from app.services.firebase_container import FirebaseContainer
+from app.shared.utils import read_img_from_path_with_mask, upload_file
 
 PROJECT_PATH = Path.cwd()
 
 
-def fill_vector_database() -> None:
+async def fill_vector_database() -> None:
     """Fill the Pinecone database with the images that are inside the caps folder."""
     pinecone_container = PineconeContainer()
+    firebase_container = FirebaseContainer()
+
     pinecone_container.empty_index()
     root_dir = str(Path("database") / "caps")
     folders = os.listdir(root_dir)
     img_vectorizer = ImageVectorizer()
-    for img_path in folders:
-        file_path: str = str(Path(root_dir) / img_path)
-        img = read_img_from_path_with_mask(file_path)
+    for indx, img_path in enumerate(folders):
+        file_path: Path = Path(root_dir) / img_path
+
+        img = read_img_from_path_with_mask(str(file_path))
         vector = img_vectorizer.numpy_to_vector(img=img)
+
+        file: UploadFile = await upload_file(file_path)
+        firebase_container.add_image_to_container(file, img_path, "test_user")
         pinecone_container.upsert_into_pinecone(
             vector_id=img_path, values=vector, metadata={"user_id": "test_user", "name": img_path}
         )
+        logger.info(f"A total of {indx}/{len(folders)} have been uploaded.")
 
 
 if __name__ == "__main__":
-    fill_vector_database()
+    asyncio.run(fill_vector_database())
