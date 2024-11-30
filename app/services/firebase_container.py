c809950729e1d0b30ea42da8b89f4df124633926
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2024-11-30 19:05:49 +0100

    Update firebase_container.py

--- app/services/firebase_container.py
+++ app/services/firebase_container.py
@@ -1,3 +1,5 @@
+import json
+
 import firebase_admin
 import starlette.status
 from fastapi import HTTPException, UploadFile
@@ -18,7 +20,7 @@ class FirebaseContainer:
         return cls._instance
 
     def _initialize(self):
-        self.cred = credentials.Certificate(settings.firebase_config_file)
+        self.cred = credentials.Certificate(self.get_firebase_credentials())
         self.app = firebase_admin.initialize_app(
             self.cred, {"storageBucket": settings.firebase_bucket}
         )
@@ -36,3 +38,11 @@ class FirebaseContainer:
         blob.upload_from_file(file.file, content_type=file.content_type)
         blob.make_public()
         return blob.public_url
+
+    @staticmethod
+    def get_firebase_credentials() -> dict:
+        """Parse firebase_credentials JSON string to a dictionary."""
+        try:
+            return json.loads(settings.firebase_config_file)
+        except json.JSONDecodeError as e:
+            raise ValueError("Invalid JSON for firebase_credentials") from e
