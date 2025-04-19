df6a87d0e5f04ad140655f2f6d558a0ac8bf76e3
C. M. Păduraru <cosminmarian.paduraru@gmail.com>
2025-04-19 12:23:28 +0200

    bugfix: update to python 3.12

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11
+FROM python:3.12
 
 WORKDIR /root/app/bottle-caps-backend
 
--- app/main.py
+++ app/main.py
@@ -70,4 +70,4 @@ def health_check(request: Request):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
